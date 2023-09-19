f00df9fe7142b3ad1638a2d6ba04bcf8ea045d79
hejiajun <hejiajun02@lizhi.fm>
2023-09-19 10:19:30 +0800

    feat : update the click theme

--- app/src/main/java/com/returntolife/accessibilityutils/FloatingClickView.kt
+++ app/src/main/java/com/returntolife/accessibilityutils/FloatingClickView.kt
@@ -3,17 +3,16 @@ package com.returntolife.accessibilityutils
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.text.TextUtils
-import android.util.Log
+import android.graphics.PixelFormat
+import android.os.Build
+import android.view.Gravity
 import android.view.LayoutInflater
-import android.view.View
-import android.view.View.OnClickListener
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.view.WindowManager
 import androidx.appcompat.app.AlertDialog
 import com.blankj.utilcode.util.LogUtils
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.returntolife.accessibilityutils.databinding.DialogMotifyClickinfoBinding
+import com.returntolife.accessibilityutils.databinding.ViewFloatingClickBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
@@ -27,15 +26,18 @@ import kotlinx.coroutines.launch
  *@date: 9/18/23
  *des:
  */
-class FloatingClickView(private val mContext: Context, val clickInfo: ClickInfo) : DragViewGroup(mContext) {
+class FloatingClickView(private val mContext: Context, val clickInfo: ClickInfo) :
+    DragViewGroup(mContext) {
 
 
     private var scope: CoroutineScope? = null
 
-    var clickListener:((Float,Float,ClickInfo)->Unit)?=null
+    var clickListener: ((Float, Float, ClickInfo) -> Unit)? = null
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.view_floating_click, this)
+        val binding = ViewFloatingClickBinding.inflate(LayoutInflater.from(context), this)
+
+        binding.tvName.text = clickInfo.id.toString()
 
         initListener()
     }
@@ -43,12 +45,13 @@ class FloatingClickView(private val mContext: Context, val clickInfo: ClickInfo)
     private var timeTemp = 0L
     private var clickCount = 1
 
-    private suspend fun checkClick(){
-        delay(100)
+    private suspend fun checkClick() {
+        //每隔16.6毫秒检测一次,即最多1秒点击60次
+        delay(17)
 
-        if(System.currentTimeMillis()-timeTemp>clickInfo.interval){
+        if (System.currentTimeMillis() - timeTemp > clickInfo.interval) {
             val intArray = getViewPos()
-            clickListener?.invoke(intArray[0].toFloat(),intArray[1].toFloat(),clickInfo)
+            clickListener?.invoke(intArray[0].toFloat(), intArray[1].toFloat(), clickInfo)
             LogUtils.d("点击 id=${clickInfo.id} count =${clickCount++}")
             timeTemp = System.currentTimeMillis()
         }
@@ -56,7 +59,7 @@ class FloatingClickView(private val mContext: Context, val clickInfo: ClickInfo)
         checkClick()
     }
 
-    fun startAutoClick(){
+    fun startAutoClick() {
         scope?.cancel()
         scope = MainScope()
         scope?.launch(Dispatchers.IO) {
@@ -64,113 +67,51 @@ class FloatingClickView(private val mContext: Context, val clickInfo: ClickInfo)
         }
     }
 
-    fun stopAutoClick(){
+    fun stopAutoClick() {
         scope?.cancel()
     }
 
 
     override fun remove() {
-        scope?.cancel()
+        stopAutoClick()
         super.remove()
 
     }
 
-
-    @SuppressLint("ClickableViewAccessibility")
     private fun initListener() {
         setOnLongClickListener {
-//
             showDialog()
             true
         }
-//
-//        mView.setOnClickListener {
-//
-//            val location = IntArray(2)
-//            it.getLocationOnScreen(location)
-//
-//            val intent = Intent().apply {
-//                action = BroadcastConstants.BROADCAST_ACTION_AUTO_CLICK
-//                when (mCurrentState) {
-//                    STATE_NORMAL -> {
-//                        mCurrentState = STATE_CLICKING
-//                        putExtra(BroadcastConstants.KEY_ACTION, AutoClickService.ACTION_PLAY)
-//                        putExtra(BroadcastConstants.KEY_POINT_X, (location[0] - 1).toFloat())
-//                        putExtra(BroadcastConstants.KEY_POINT_Y, (location[1] - 1).toFloat())
-////                        ivIcon?.setImageResource(R.drawable.ic_auto_click_icon_green_24)
-//                    }
-//                    STATE_CLICKING -> {
-//                        mCurrentState = STATE_NORMAL
-//                        putExtra(BroadcastConstants.KEY_ACTION, AutoClickService.ACTION_STOP)
-////                        ivIcon?.setImageResource(R.drawable.ic_auto_click_icon_gray_24)
-//                    }
-//                }
-//            }
-//
-//            context.sendBroadcast(intent)
-//        }
     }
 
 
     private fun showDialog() {
-        val builder = AlertDialog.Builder(mContext)
-        val dialog = builder.create()
+        val dialogBinding =
+            DialogMotifyClickinfoBinding.inflate(LayoutInflater.from(mContext.applicationContext))
+        dialogBinding.etInterval.setText(clickInfo.interval.toString())
+        dialogBinding.etCount.setText(clickInfo.clickCount.toString())
 
-        val dialogBinding = DialogMotifyClickinfoBinding.inflate(LayoutInflater.from(mContext))
-        //设置对话框布局
-        dialog.setView(dialogBinding.root)
+        val dialog = android.app.AlertDialog.Builder(mContext.applicationContext)
+            .setView(dialogBinding.root)
+            .create()
+
+        val flag: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        } else {
+            WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
+        }
+        dialog.window?.setType(flag)
         dialog.show()
 
-        dialogBinding.etInterval.setText(clickInfo.interval)
-        dialogBinding.etCount.setText(clickInfo.clickCount)
 
-        dialogBinding.btnOk.setOnClickListener(OnClickListener {
+        dialogBinding.btnOk.setOnClickListener {
             clickInfo.interval = dialogBinding.etInterval.text.toString().toInt()
             clickInfo.clickCount = dialogBinding.etCount.text.toString().toInt()
 
             dialog.dismiss()
-        })
+        }
 
     }
-
-
-//    fun getViewPos():IntArray{
-//        val location = IntArray(2)
-//        getLocationOnScreen(location)
-//        return location
-//    }
-//
-//    fun show() {
-//        mParams = WindowManager.LayoutParams()
-//        mParams?.apply {
-//            gravity = Gravity.CENTER
-//            //总是出现在应用程序窗口之上
-//            type = if (Build.VERSION.SDK_INT >= 26) {
-//                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
-//            } else {
-//                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
-//            }
-//            //设置图片格式，效果为背景透明
-//            format = PixelFormat.RGBA_8888
-//
-//            flags =
-//                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
-//                        WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR or
-//                        WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
-//
-//            width = LayoutParams.WRAP_CONTENT
-//            height = LayoutParams.WRAP_CONTENT
-//            if (mView.isAttachedToWindow) {
-//                mWindowManager.tryRemoveView(mView)
-//            }
-//            mWindowManager.tryAddView(mView, this)
-//        }
-//    }
-//
-//    fun remove() {
-//        mCurrentState = STATE_NORMAL
-////        ivIcon?.setImageResource(R.drawable.ic_auto_click_icon_gray_24)
-//        mWindowManager.tryRemoveView(mView)
-//    }
 
 }
--- app/src/main/java/com/returntolife/accessibilityutils/MenuManager.kt
+++ app/src/main/java/com/returntolife/accessibilityutils/MenuManager.kt
@@ -3,21 +3,15 @@ package com.returntolife.accessibilityutils
 import android.content.Context
 import android.view.LayoutInflater
 import com.returntolife.accessibilityutils.databinding.ViewMenuBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 /**
  *@author: hejiajun02@lizhi.fm
  *@date: 9/18/23
  *des:
  */
-class MenuManager(private val context:Context) {
+class MenuManager(private val context: Context) {
 
-    private var menuBinding:ViewMenuBinding?=null
+    private var menuBinding: ViewMenuBinding? = null
 
     private val clickInfoList = ArrayList<FloatingClickView>()
 
@@ -25,10 +19,10 @@ class MenuManager(private val context:Context) {
     @Volatile
     private var isPlaying = false
 
-     var clickListener:((Float,Float,ClickInfo)->Unit)?=null
+    var clickListener: ((Float, Float, ClickInfo) -> Unit)? = null
 
 
-    fun showMenuManager(){
+    fun showMenuManager() {
         menuBinding = ViewMenuBinding.inflate(LayoutInflater.from(context))
         menuBinding!!.root.show()
 
@@ -36,21 +30,25 @@ class MenuManager(private val context:Context) {
     }
 
 
-
     private fun initListener() {
         menuBinding?.let {
             it.ivAdd.setOnClickListener {
 
-                val clickInfo = ClickInfo(clickInfoList.size+1,5000*(clickInfoList.size+1),1)
+                val clickInfo =
+                    ClickInfo(clickInfoList.size + 1, 1000)
 
-                FloatingClickView(context,clickInfo).apply {
+                FloatingClickView(context, clickInfo).apply {
                     clickInfoList.add(this)
                     this.show()
                 }
             }
 
             it.ivRemove.setOnClickListener {
-
+                if (clickInfoList.size > 0) {
+                    val view = clickInfoList[clickInfoList.size - 1];
+                    view.remove()
+                    clickInfoList.remove(view)
+                }
             }
 
             it.ivClose.setOnClickListener {
@@ -58,18 +56,18 @@ class MenuManager(private val context:Context) {
             }
 
             it.ivPlay.setOnClickListener {
-                if(isPlaying){
+                if (isPlaying) {
                     isPlaying = false
                     menuBinding?.ivPlay?.setImageResource(R.mipmap.ic_play)
-                    clickInfoList.forEach {
-                        it.stopAutoClick()
+                    clickInfoList.forEach { view ->
+                        view.stopAutoClick()
                     }
-                }else{
+                } else {
                     isPlaying = true
                     menuBinding?.ivPlay?.setImageResource(R.mipmap.ic_pause)
 
-                    clickInfoList.forEach {
-                      it.startAutoClick()
+                    clickInfoList.forEach { view ->
+                        view.startAutoClick()
                     }
                 }
             }
@@ -77,9 +75,7 @@ class MenuManager(private val context:Context) {
     }
 
 
-
-
-    fun remove(){
+    fun remove() {
         clickInfoList.forEach {
             it.stopAutoClick()
         }
